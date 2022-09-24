b44d672917b837da9dd637b1be95f1f669ddcf5d
Julian Canepa <julian.l.canepa@gmail.com>
2022-09-24 09:50:17 -0700

    Add comments to true/false tests, passing null treated as false.

--- about_true_and_false.rb
+++ about_true_and_false.rb
@@ -9,18 +9,22 @@ class AboutTrueAndFalse < Neo::Koan
     end
   end
 
+  # true should be true.
   def test_true_is_treated_as_true
     assert_equal :true_stuff, truth_value(true)
   end
 
+  # false should be false.
   def test_false_is_treated_as_false
     assert_equal :false_stuff, truth_value(false)
   end
 
+  # nil (null) should be false.
   def test_nil_is_treated_as_false_too
     assert_equal :false_stuff, truth_value(nil)
   end
 
+  # any value should be true.
   def test_everything_else_is_treated_as_true
     assert_equal __, truth_value(1)
     assert_equal __, truth_value(0)
