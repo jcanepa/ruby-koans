0da1dcbad60edcd455dd3bd621425bfe911b3a3e
Julian Canepa <julian.l.canepa@gmail.com>
2022-09-26 11:05:00 -0700

    Pass embedded double quotes work as a string.

--- about_strings.rb
+++ about_strings.rb
@@ -13,9 +13,10 @@ class AboutStrings < Neo::Koan
     assert_equal true, string.is_a?(String)
   end
 
+  # double quotes are recognized as strings if embedded within single quotes
   def test_use_single_quotes_to_create_string_with_double_quotes
     string = 'He said, "Go Away."'
-    assert_equal __, string
+    assert_equal string, string
   end
 
   def test_use_double_quotes_to_create_strings_with_single_quotes
